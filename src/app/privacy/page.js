fe1151408d68a9baa214c3ea39880ab0667d6bbf
ardacbrglu <arda.cabaroglu@sabanciuniv.edu>
2025-08-06 00:45:03 +0300

    major fixes on register-login pages password set and reset5

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -1,19 +1,9 @@
-// app/terms_privacy/page.js
-export default function TermsPrivacyPage() {
-  return (
-    <div className="p-12 text-center text-lg text-gray-200">
-      <h1>Terms & Privacy</h1>
-      <p>Coming soon...</p>
-    </div>
-  );
-}
+'use client';
 
-// app/faq/page.js
-export default function FaqPage() {
+export default function Page() {
   return (
-    <div className="p-12 text-center text-lg text-gray-200">
-      <h1>FAQ</h1>
-      <p>Frequently Asked Questions coming soon...</p>
+    <div>
+      Coming soon.
     </div>
   );
 }
