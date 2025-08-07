6d6af363dfb0866bc0b4c8806a7f72346c5df260
ardacbrglu <arda.cabaroglu@sabanciuniv.edu>
2025-08-07 18:50:24 +0300

    google register login fix3

--- src/app/check-google-signin/page.js
+++ src/app/check-google-signin/page.js
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
