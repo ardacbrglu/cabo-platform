7ba49b2ff810f9ca71a33db8d7e534194a1f62f3
ardacbrglu <arda.cabaroglu@sabanciuniv.edu>
2025-08-07 18:59:55 +0300

    google register login fix5

--- src/app/check-google-signin/page.js
+++ src/app/check-google-signin/page.js
@@ -1,8 +1,5 @@
-export const dynamic = "force-dynamic";
-
 import dynamic from "next/dynamic";
 
-// Client bileşeni ayrı dosyaya alıyoruz
 const ClientCheck = dynamic(() => import("./ClientCheck"), {
   ssr: false,
 });
