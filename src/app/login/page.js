7f46d16a9b1f2d41838f422e63e9e4590af3bca3
ardacbrglu <arda.cabaroglu@sabanciuniv.edu>
2025-08-07 20:05:49 +0300

    google register login change2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { signIn } from 'next-auth/react';
 import PublicLayout from '@/components/PublicLayout';
